ee945911150ee4f00e461fe083dbddfa28273cab
vladimir_tim <tko.vladimir@gmail.com>
2019-03-11 23:03:30 +0300

    very final version

--- brackets_balance.c
+++ brackets_balance.c
@@ -1,7 +1,6 @@
 /*
 
 ИЗ1 Тимошенко Владимир АПО-12
-Run ID: 590
 
 Составить программу построчной обработки текста. Суть обработки - отбор строк,
 содержащих одинаковое количество открывающих и закрывающих круглых скобок.
@@ -33,30 +32,32 @@ Run ID: 590
 #define NONE -1
 
 
-int check_string(char const *const str);
+_Bool check_brackets_balance_in_string(char const *const str);
 size_t count_strings(char const *const *const string_array, const size_t size,
                      char ***result_array);
-char **read_array_from_stdin(size_t *size);
+void reallocation(char *** arr, size_t *string_array_size, _Bool *allocation_error);
+char **read_array_from_input(FILE *stream, size_t *size);
 
 
 int main() {
   size_t size = 0;
 
-  char **string_array = read_array_from_stdin(&size);
+  char **string_array = read_array_from_input(stdin, &size);
   if (!string_array) {
-    printf("[Error]");
+    printf("[error]");
     return 0;
   }
 
-  char **result_array;
+  char **result_array = NULL;
 
   size_t new_size = count_strings((char const *const *const)string_array, size,
                                   &result_array);
 
   if (!result_array) {
-    printf("[Error]");
-    for (size_t i = 0; i < size; ++i)
+    printf("[error]");
+    for (size_t i = 0; i < size; ++i) {
       free(string_array[i]);
+    }
     free(string_array);
     return 0;
   }
@@ -80,9 +81,9 @@ int main() {
 }
 
 
-int check_string(char const *const str) {
+_Bool check_brackets_balance_in_string(char const *const str) {
   if (str == NULL)
-    return 0;
+    return false;
   int brackets_sum = 0;
   int i = 0;
   while (str[i] != '\0') {
@@ -111,115 +112,112 @@ size_t count_strings(char const *const *const string_array, const size_t size,
   if (!result_array)
     return 0;
 
-  size_t j = 0;
+  size_t result_array_it = 0;
   size_t result_array_size = DEFAULT_ARR_SIZE;
-  int allocation_error = false;
+  _Bool allocation_error = false;
 
   for (size_t i = 0; i < size; ++i) {
-    if (j == result_array_size - 1) {
-      char **new_result_array = (char **)realloc(
-          *result_array, result_array_size * 2 * sizeof(char *));
+    if (check_brackets_balance_in_string(string_array[i])) {
+      if (result_array_it == result_array_size - 1) {
+        reallocation(result_array, &result_array_size, &allocation_error);
+      }
 
-      if (!new_result_array) {
+      (*result_array)[result_array_it] = (char *)malloc((strlen(string_array[i]) + 1));
+
+      if (!(*result_array)[result_array_it]) {
         allocation_error = true;
         break;
       }
 
-      *result_array = new_result_array;
-      result_array_size *= 2;
-    }
-
-    if (check_string(string_array[i])) {
-      (*result_array)[j] = (char *)malloc((strlen(string_array[i]) + 1));
-
-      if (!(*result_array)[j]) {
-        allocation_error = true;
-        break;
-      }
-
-      strcpy((*result_array)[j], string_array[i]);
-      ++j;
+      strcpy((*result_array)[result_array_it], string_array[i]);
+      ++result_array_it;
     }
   }
 
   if (allocation_error) {
-    for (size_t k = 0; k < j; ++k)
-      free((*result_array)[k]);
+    for (size_t i = 0; i < result_array_it; ++i) {
+      free((*result_array)[i]);
+    }
     free(*result_array);
     *result_array = NULL;
   }
 
-  return j;
+  return result_array_it;
 }
 
-char **read_array_from_stdin(size_t *size) {
+char **read_array_from_input(FILE *stream, size_t *size) {
   if (!size)
     return NULL;
 
-  int string_array_size = DEFAULT_ARR_SIZE;
+  size_t string_array_size = DEFAULT_ARR_SIZE;
   char **arr = (char **)malloc(string_array_size * sizeof(char *));
 
   if (!arr) {
     return NULL;
   }
 
-  char buffer[BUFFERSIZE];
-  int i = 0;
-  int i_previous = NONE;
-  int multiplicator = DEFAULT_MULT;
-  int allocation_error = false;
+  char buffer[BUFFERSIZE] = {0};
+  int string_array_it = 0;
+  int it_previous = NONE;
+  size_t new_str_size = 2 * BUFFERSIZE;
+  _Bool allocation_error = false;
 
-  while (fgets(buffer, BUFFERSIZE, stdin)) {
-    if (i == string_array_size - 1) {
-      string_array_size *= 2;
-      char **new_arr =
-          (char **)realloc(arr, sizeof(char *) * string_array_size);
-
-      if (!new_arr) {
-        allocation_error = true;
-        break;
-      }
-
-      arr = new_arr;
+  while (fgets(buffer, BUFFERSIZE, stream)) {
+    if (string_array_it == string_array_size - 1) {
+      reallocation(&arr, &string_array_size, &allocation_error);
     }
 
-    if (i != i_previous) {
-      arr[i] = (char *)calloc(BUFFERSIZE, sizeof(char *));
+    if (string_array_it != it_previous) {
+      arr[string_array_it] = (char *)calloc(BUFFERSIZE, sizeof(char *));
 
-      if (!arr[i]) {
+      if (!arr[string_array_it]) {
         allocation_error = true;
         break;
       }
     }
 
     if (strlen(buffer) != BUFFERSIZE - 1 || buffer[strlen(buffer) - 1] == '\n') {
-      strcat(arr[i], buffer);
-      if (arr[i][strlen(arr[i]) - 1] == '\n')
-        arr[i][strlen(arr[i]) - 1] = '\0';
-      multiplicator = DEFAULT_MULT; //значение множителя возвращается к 2 во избежание выделения
-      ++i;                          //строк чрезвычайно большого размера(выделяем линейно,если надо)
+      strcat(arr[string_array_it], buffer);
+      if (arr[string_array_it][strlen(arr[string_array_it]) - 1] == '\n')
+        arr[string_array_it][strlen(arr[string_array_it]) - 1] = '\0';
+      new_str_size = 2 * BUFFERSIZE;
+      ++string_array_it;
     } else {
-      char *p_arr_new = (char *)realloc(arr[i], BUFFERSIZE * multiplicator);
+      char *p_arr_new = (char *)realloc(arr[string_array_it], new_str_size);
 
       if (!p_arr_new) {
         allocation_error = true;
         break;
       }
 
-      arr[i] = p_arr_new;
-      strcat(arr[i], buffer);
-      ++multiplicator; //мультипликатор увеличивается линейно,
-      i_previous = i;  //так как буфер остается прежнего размера
+      arr[string_array_it] = p_arr_new;
+      strcat(arr[string_array_it], buffer);
+      new_str_size += BUFFERSIZE;
+      it_previous = string_array_it;
     }
   }
 
   if (allocation_error) {
-    for (size_t j = 0; j < i; ++j)
-      free(arr[j]);
+    for (size_t i = 0; i < string_array_it; ++i) {
+      free(arr[i]);
+    }
     free(arr);
+    
     return NULL;
   }
 
-  *size = i;
+  *size = string_array_it;
   return arr;
+}
+
+void reallocation(char ***arr, size_t *string_array_size, _Bool *allocation_error) {
+  *string_array_size *= DEFAULT_MULT;
+  char **new_arr =
+    (char **)realloc(*arr, *string_array_size * sizeof(char *));
+
+  if (!new_arr) {
+    *allocation_error = true;
+  }
+
+  *arr = new_arr;
 }
